b19c34e91af941602da7fcc4a813ee794dfb0c1c
momomeomo <momomeomo@hotmail.com>
2018-11-25 15:02:41 -0800

    updated

--- buildpy.bat
+++ buildpy.bat
@@ -1,10 +1,7 @@
 @echo off
 
 :start
-	REM cd C:\Users\momom\Desktop\Repo\Calculator
-	REM python calc.py
+python Main.py
 
-set choice=
-set /p choice="Do you want to restart? Press 'y' and enter for Yes: "
-if not '%choice%'=='' set choice=%choice:~0,1%
-if '%choice%'=='y' goto start
+set /p DUMMY=Hit ENTER to restart...
+goto start
