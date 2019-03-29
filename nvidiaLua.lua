222ff905e0df265b69776813670f7c8bc5eac5b6
momomeomo <momomeomo@hotmail.com>
2019-03-29 14:46:48 -0700

    removed extra selenium scripts

--- nvidiaLua.lua
+++ nvidiaLua.lua
@@ -1,14 +1,16 @@
 -- opens nvidia control panel + afterburner from their default locations
 
 function sleep(n)
-  if 
+  if
       n > 0 then os.execute("ping -n " .. tonumber(n+1) .. " localhost > NUL")
   end
 end
 
 
 io.write("continue with this operation (y/n)?")
+
 answer=io.read()
+
 if answer=="y" then
   print("opening Nvidia")
   os.execute('"C:\\Program Files\\NVIDIA Corporation\\Control Panel Client\\nvcplui.exe"', "r")
@@ -17,10 +19,10 @@ if answer=="y" then
   os.execute('"C:\\Program Files (x86)\\MSI Afterburner\\MSIAfterburner.exe"', "r")
 elseif answer=="n" then
   print("closing")
-  sleep(1)
   print("...")
   sleep(1)
   print("..")
   sleep(1)
   print(".")
+  sleep(1)
 end
