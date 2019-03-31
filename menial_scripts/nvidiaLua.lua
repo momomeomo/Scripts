2b2c678e4c9bb963173e09f1b1ed1c7e698f8952
momomeomo <momomeomo@hotmail.com>
2019-03-31 10:24:26 -0700

    rtss check

--- menial_scripts/nvidiaLua.lua
+++ menial_scripts/nvidiaLua.lua
@@ -1,10 +1,31 @@
 -- opens nvidia control panel + afterburner from their default locations
 
+-- sleep function
 function sleep(n)
   if
       n > 0 then os.execute("ping -n " .. tonumber(n+1) .. " localhost > NUL")
   end
 end
+
+-- powershell script
+local script = [[
+
+if((get-process "RTSS" -ea SilentlyContinue) -eq $Null){
+
+	"MSIAfterburner Not Running"
+
+
+}	else	{
+
+    "MSIAfterburner Running"
+
+ }
+
+]]
+-- create powershell process and feed script to its stdin
+local pipe = io.popen("powershell -command -", "w")
+pipe:write(script)
+pipe:close()
 
 
 io.write("continue with this operation (y/n)?")
@@ -19,10 +40,6 @@ if answer=="y" then
   os.execute('"C:\\Program Files (x86)\\MSI Afterburner\\MSIAfterburner.exe"', "r")
 elseif answer=="n" then
   print("closing")
-  print("...")
-  sleep(1)
-  print("..")
-  sleep(1)
-  print(".")
+  print(".......")
   sleep(1)
 end
