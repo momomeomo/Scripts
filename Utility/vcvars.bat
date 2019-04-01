01d0ef97a72277ffeb8ed1409ef53597e06ccea4
momomeomo <momomeomo@hotmail.com>
2019-04-01 14:45:51 -0700

    moved old scripts

--- Utility/vcvars.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-Call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\vsdevcmd\ext\vcvars.bat"
-Call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\vsdevcmd\ext\vcvars140.bat"
